ac21cbb8381242144fd538a2a7db67689b22f8aa
martindiazgs <145728132+martindiazgs@users.noreply.github.com>
2023-11-02 20:55:42 -0500

    Add files via upload

---
+++ JUEGO/js/juego.js
@@ -0,0 +1,29 @@
+
+let posicion=Math.floor(Math.random() * 3);
+function encuentre(num){
+  
+  if(posicion==num){
+    if(num==0){
+      document.getElementById("b0").src="img/bolita.png";
+
+    }
+    else{
+      if(num==1){
+        document.getElementById("b1").src="img/bolita.png";
+
+      }
+      else{
+        document.getElementById("b2").src="img/bolita.png";
+      }
+    }
+    
+  }
+  else{
+    alert("Intentelo de nuevo")
+  
+  }
+  
+  
+  
+}
+
